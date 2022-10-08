441cec76db7e9f03a15f24f3ccc15c0c573005d3
cod3burrito <robintanner44@gmail.com>
2022-10-08 14:17:48 -0400

    displaying prompts correctly! yay!

--- index.js
+++ index.js
@@ -5,34 +5,41 @@ const { off } = require("process");
     
 // TODO: Create an array of questions for user input
 inquirer
-.prompt([])
-const questions = [
-    {
-        type: "input",
-        name: "username",
-        message: "What's your GitHub username?"
-    }, {
-        type: "input",
-        name: "project name",
-        message: "What is your coding project?"
-    }, {
-        type: "input",
-        name: "description",
-        message: "Tell us about your project!"
-    }, {
-        type: "input",
-        name: "install",
-        message: "Tell us how to install it."
-    }, {
-        type: "input",
-        name: "usage",
-        message: "How do we need to use it?"
-    }, {
-        type: "input",
-        name: "credit",
-        message: "Are there any sources to cite? Any collaborators on the project?"
-    }
-];
+.prompt([
+    // const questions = 
+
+        {
+            type: "input",
+            name: "username",
+            message: "What's your GitHub username?"
+        }, {
+            type: "input",
+            name: "project_name",
+            message: "What is your coding project?"
+        }, {
+            type: "input",
+            name: "description",
+            message: "Tell us about your project!"
+        }, {
+            type: "input",
+            name: "install",
+            message: "Tell us how to install it."
+        }, {
+            type: "input",
+            name: "usage",
+            message: "How do we need to use it?"
+        }, {
+            type: "input",
+            name: "credit",
+            message: "Are there any sources to cite? Any collaborators on the project?"
+        }
+    
+])
+.then((data) => {
+    const filename = `${data.project_name.toLowerCase()+ 'README.md'}.json`;
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!'))
+});
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -40,7 +47,7 @@ function writeToFile(fileName, data) {
         if(error) {
             return console.log(error);
         }
-        console.log("Got it! Your information is saved!");
+        return console.log("Got it! Your information is saved!");
     })
 }
 
